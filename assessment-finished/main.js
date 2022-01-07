01c3f030ee132dac8e0b856ed565f56ac232336e
kimu9 <95006410+kimu9@users.noreply.github.com>
2022-01-07 11:47:50 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -8,10 +8,10 @@ function randomValueFromArray(array){
 }
 
 //const storyText = 'その日は、とても暑かった。 so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-const storyText = '気温 :insertx: で外は寒かった。:inserty:は出かけた。:insertz:に着いた。雪が降っていた。体重50kgの:inserty:は驚いたが、津田は驚かなかった。:insertz:ではよくあることだった。';
-const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
-const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+const storyText = '気温摂氏-1で外は寒かった。:insertx:は出かけた。:inserty:に着いた。:insertz:。体重50kgの :insertx: は驚いたが、津田は驚かなかった。 :inserty: ではよくあることだった。';
+const insertX = ['スヌーピー', 'ミッキーマウス', 'サンタクロース'];
+const insertY = ['ディズニーランド', '千駄ヶ谷', '津田塾大学'];
+const insertZ = ['雪が降っていた', '混雑していた', '雨が降っていた'];
 
 randomize.addEventListener('click', result);
 
