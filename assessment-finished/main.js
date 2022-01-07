5cd93f45a04bcae02deee45a65e2a74a132fae77
kimu9 <95006410+kimu9@users.noreply.github.com>
2022-01-07 11:31:06 +0900

    Create main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -9,6 +9,7 @@ function randomValueFromArray(array){
 
 //const storyText = 'その日は、とても暑かった。 so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 const storyText = '気温 :insertx: で外は寒かった。:inserty:は出かけた。:insertz:に着いた。雪が降っていた。体重50kgの:inserty:は驚いたが、津田は驚かなかった。:insertz:ではよくあることだった。';
+const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
